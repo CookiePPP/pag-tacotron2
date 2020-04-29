48f1acd973e30c7b7bfa27dd1190cea17c7aef01
Cookie <42448678+CookiePPP@users.noreply.github.com>
2020-04-30 00:31:44 +0100

    fix Validation loss

--- train.py
+++ train.py
@@ -132,7 +132,7 @@ def validate(model, criterion, valset, iteration, batch_size, n_gpus,
         for i, batch in enumerate(val_loader):
             x, y = model.parse_batch(batch)
             y_pred = model(x)
-            loss = criterion(y_pred, y)
+            loss, attention_loss_item = criterion(y_pred, y)
             if distributed_run:
                 reduced_val_loss = reduce_tensor(loss.data, n_gpus).item()
             else:
@@ -142,7 +142,7 @@ def validate(model, criterion, valset, iteration, batch_size, n_gpus,
 
     model.train()
     if rank == 0:
-        print("Validation loss {}: {:9f}  ".format(iteration, val_loss))
+        print("Validation loss {}: Spectrogram {:9f}  Attention {:9f}".format(iteration, val_loss))
         logger.log_validation(val_loss, model, y, y_pred, iteration)
 
 
