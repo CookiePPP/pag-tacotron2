cf048dbd30148e8a77cadcc066c9a22cbe3e5134
Cookie <42448678+CookiePPP@users.noreply.github.com>
2020-04-29 19:19:06 +0100

    add `alignment loss` and `pos_weight` to loss_function.py

--- loss_function.py
+++ loss_function.py
@@ -2,18 +2,21 @@ from torch import nn
 
 
 class Tacotron2Loss(nn.Module):
-    def __init__(self):
+    def __init__(self, hparams):
         super(Tacotron2Loss, self).__init__()
-
+        self.pos_weight = torch.tensor(hparams.gate_positive_weight)
+        self.alignment_loss_scaler = hparams.alignment_loss_weight
+    
     def forward(self, model_output, targets):
-        mel_target, gate_target = targets[0], targets[1]
+        mel_target, gate_target, alignment_target = targets
         mel_target.requires_grad = False
         gate_target.requires_grad = False
         gate_target = gate_target.view(-1, 1)
 
-        mel_out, mel_out_postnet, gate_out, _ = model_output
+        mel_out, mel_out_postnet, gate_out, alignment_out = model_output
         gate_out = gate_out.view(-1, 1)
         mel_loss = nn.MSELoss()(mel_out, mel_target) + \
             nn.MSELoss()(mel_out_postnet, mel_target)
-        gate_loss = nn.BCEWithLogitsLoss()(gate_out, gate_target)
-        return mel_loss + gate_loss
+        alignment_loss = nn.MSELoss()(alignment_out, alignment_target)
+        gate_loss = nn.BCEWithLogitsLoss(pos_weight=self.pos_weight)(gate_out, gate_target)
+        return mel_loss + gate_loss + (self.alignment_loss_scaler * alignment_loss), alignment_loss.item()
