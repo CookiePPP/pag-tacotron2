8deabc1072c7091dd746b1108c0caecabcd85dde
Cookie <42448678+CookiePPP@users.noreply.github.com>
2020-04-30 02:48:19 +0100

    add optional encoder dim Attention mean

--- loss_function.py
+++ loss_function.py
@@ -6,6 +6,7 @@ class Tacotron2Loss(nn.Module):
         super(Tacotron2Loss, self).__init__()
         self.pos_weight = torch.tensor(hparams.gate_positive_weight)
         self.alignment_loss_scaler = hparams.alignment_loss_weight
+        self.alignment_encoderwise_mean = hparams.alignment_encoderwise_mean
     
     def forward(self, model_output, targets):
         mel_target, gate_target, alignment_target = targets
@@ -17,6 +18,10 @@ class Tacotron2Loss(nn.Module):
         gate_out = gate_out.view(-1, 1)
         mel_loss = nn.MSELoss()(mel_out, mel_target) + \
             nn.MSELoss()(mel_out_postnet, mel_target)
-        alignment_loss = nn.MSELoss()(alignment_out.transpose(1,2), alignment_target)
+        if self.alignment_encoderwise_mean:
+            alignment_loss = nn.MSELoss()(alignment_out.transpose(1,2), alignment_target)
+        else
+            alignment_loss = nn.MSELoss(reduction='sum')(alignment_out.transpose(1,2), alignment_target)
+            alignment_loss = alignment_loss / (alignment_target.shape[0]*alignment_target.shape[2])
         gate_loss = nn.BCEWithLogitsLoss(pos_weight=self.pos_weight)(gate_out, gate_target)
         return mel_loss + gate_loss + (self.alignment_loss_scaler * alignment_loss), alignment_loss.item()
